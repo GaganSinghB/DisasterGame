29c7582a7e88736e04205b85c7ab18a79706bfe6
GaganSinghB <gagansingh020702@gmail.com>
2021-08-26 17:23:04 +0530

    Update CameraController.cs

--- Test Project 2D/Assets/Scripts/CameraController.cs
+++ Test Project 2D/Assets/Scripts/CameraController.cs
@@ -20,13 +20,13 @@ public class CameraController : MonoBehaviour
         halfwidth = halfheight * Camera.main.aspect;
 
         bottomLeftLimit = Map.localBounds.min + new Vector3(halfwidth, halfheight, 0f);
-        topRightLimit = Map.localBounds.max - new Vector3(halfwidth, halfheight,0f);
-    }
+        topRightLimit = Map.localBounds.max - new Vector3(halfwidth, halfheight, 0f);
+    } 
 
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y - 0.3f , 10f), transform.position.z);
     }
 }
